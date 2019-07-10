dbfef8b3bbfb266d1ee81e6f1187bae7cc0c0afb
Ivan Chub <ivansergchub@gmail.com>
2019-07-10 14:09:28 -0700

    added loading of static files

--- index.ts
+++ index.ts
@@ -4,3 +4,22 @@ console.log(analysis);
 
 const canvas: HTMLCanvasElement = document.querySelector("#the-canvas");
 const ctx = canvas.getContext("2d");
+
+import createPlayer from "web-audio-player";
+
+var audio = createPlayer("/song.mp3");
+
+audio.on("load", () => {
+  console.log("Audio loaded...");
+
+  // start playing audio file
+  audio.play();
+
+  // and connect your node somewhere, such as
+  // the AudioContext output so the user can hear it!
+  audio.node.connect(audio.context.destination);
+});
+
+audio.on("ended", () => {
+  console.log("Audio ended...");
+});
