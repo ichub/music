8a6c49ad8365701e0dd9b02abfaf41769e4de959
Ivan Chub <ivansergchub@gmail.com>
2019-07-10 14:29:25 -0700

    able to find the current segment based on the time

--- index.ts
+++ index.ts
@@ -30,6 +30,14 @@ interface Segment {
   timbre: number[];
 }
 
+interface Context {
+  timeStart: Date;
+  currentTime: Date;
+  msSinceStart: number;
+  framesSinceStart: number;
+  currentSegmentIndex: number;
+}
+
 const canvas: HTMLCanvasElement = document.querySelector("#the-canvas");
 const ctx = canvas.getContext("2d");
 const a = analysis as Analysis;
@@ -38,12 +46,48 @@ import createPlayer from "web-audio-player";
 
 var audio = createPlayer("/song.mp3");
 
+function updateContext(c: Context, analysis: Analysis) {
+  c.currentTime = new Date();
+  c.msSinceStart = c.currentTime.getTime() - c.timeStart.getTime();
+  c.framesSinceStart++;
+
+  // find the current segment
+  while (
+    c.msSinceStart / 1000 > analysis.segments[c.currentSegmentIndex].start &&
+    analysis.segments.length > c.currentSegmentIndex
+  ) {
+    c.currentSegmentIndex++;
+  }
+}
+
 audio.on("load", () => {
-  console.log("Audio loaded...");
-  audio.play();
   audio.node.connect(audio.context.destination);
+
+  const context = {
+    timeStart: new Date(),
+    currentTime: new Date(),
+    msSinceStart: 0,
+    framesSinceStart: 0,
+    currentSegmentIndex: 0
+  };
+
+  console.log(analysis);
+
+  function start() {
+    setInterval(() => {
+      frame(context, analysis);
+      updateContext(context, analysis);
+    }, 1000 / 60);
+    audio.play();
+  }
+
+  start();
 });
 
 audio.on("ended", () => {
   console.log("Audio ended...");
 });
+
+function frame(context: Context, analysis: Analysis) {
+  console.log(`current segment idx ${context.currentSegmentIndex}`);
+}
